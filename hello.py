ab14b08b53b78563e22a4a0bed0f4d6d0deab275
GunnOchaphan <gunnochaphan@gmail.com>
2025-08-29 12:53:12 +0200

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,28 +1,24 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
+    page_title="ATIX Labs",
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Atix Labs 👋")
 
-st.sidebar.success("Select a demo above.")
+st.sidebar.success("Select tools above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    Atix Labs - Data Science and AI Solutions for Enterprises
+    - Analyze the past
+    - Automate and optimize the present
+    - Predict the future
+
+    Case Studies:
+    - [Keeps you up-to-date with ADs and SBs to ensure airwothiness.](https://www.atixlabs.com/case-studies/predictive-maintenance)
+    - [Reduce Ground-time by eliminating repetitive tasks](https://www.atixlabs.com/case-studies/robotic-process-automation)
+    - [Process documents with AI within a matter of minutes, not days](https://www.atixlabs.com/case-studies/document-automation) ]
 """
 )
